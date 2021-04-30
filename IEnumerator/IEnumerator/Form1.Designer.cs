6dd440ddd2302a047cbd5a586fe84cd192242058
Sadigzade Mikayil <75858136+mikayil1551@users.noreply.github.com>
2021-04-30 15:11:31 +0300

    Add files via upload

---
+++ IEnumerator/IEnumerator/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace IEnumerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOyuncu = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblOyuncu
+            // 
+            this.lblOyuncu.AutoSize = true;
+            this.lblOyuncu.Location = new System.Drawing.Point(12, 31);
+            this.lblOyuncu.Name = "lblOyuncu";
+            this.lblOyuncu.Size = new System.Drawing.Size(44, 13);
+            this.lblOyuncu.TabIndex = 0;
+            this.lblOyuncu.Text = "Oyuncu";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(181, 28);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(75, 66);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(181, 186);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(295, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Oyunculari Listele";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(295, 66);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(168, 186);
+            this.listBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 267);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblOyuncu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOyuncu;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ IEnumerator/IEnumerator/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IEnumerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Takim tkm = new Takim();
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Oyuncu oyn = new Oyuncu();
+            oyn.Adi = textBox1.Text;
+            oyn.DogumTarihi = DateTime.Now;
+            oyn.FormaNo = 9;
+            oyn.Soyadi = "Sadigzade";
+            oyn.Mevkisi = "Orta saha";
+            oyn.Uyrugu = "Azerbaycan";
+            tkm.Oyuncular.Add(oyn);
+            listBox1.Items.Add(oyn.Adi);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tkm.Adi = "Besiktas";
+            tkm.KurulusYili = Convert.ToDateTime("01.01.1930");
+            tkm.StadAdi = "Vodafone";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
